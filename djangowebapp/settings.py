bf6502b6ce853b1b5b9a8537e6b2c52ee07128b0
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-04-06 02:44:32 -0400

    Add files via upload

--- djangowebapp/settings.py
+++ djangowebapp/settings.py
@@ -25,6 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Set this when using localhost
+#DEBUG = True
+
+# Set this in production
 DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['latinnomad.herokuapp.com']
